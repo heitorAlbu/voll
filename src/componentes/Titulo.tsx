29645380721e18170b82c7c65332a14ea1e9fcd8
Heitor <heitor.albuquerque@ferreiracosta.com.br>
2024-05-02 19:31:16 -0300

    adicionando multiplas propriedades na props do componente titulo

--- src/componentes/Titulo.tsx
+++ src/componentes/Titulo.tsx
@@ -1,13 +1,14 @@
 import { VStack, Image, Text, Box, FormControl, Input, Button, Link } from 'native-base'
 import { TituloProps } from '../interfaces/tituloProps'
 
-export function Titulo({ children }: TituloProps) {
+export function Titulo({ children, ...rest }: TituloProps) {
     return (
         <Text fontSize={"2xl"}
             fontWeight={"bold"}
             color="gray.500"
             textAlign="center"
-            mt={5} >
+            mt={5}
+            {...rest} >
             Faça login em sua conta
         </Text>
     )
